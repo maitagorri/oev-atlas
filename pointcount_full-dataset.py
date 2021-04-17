9cea24b2e071c9a06710a170c7fd2c3b06f9a32d
Maita Schade <maita.schade@agora-verkehrswende.de>
2021-04-17 22:01:09 +0200

    minor changes trying to figure out what is going on with 0-KFL-areas

--- pointcount_full-dataset.py
+++ pointcount_full-dataset.py
@@ -6,73 +6,86 @@ Created on Thu Mar  4 20:51:56 2021
 @author: maita
 """
 
-import fiona
+
 import pandas as pd
 import geopandas as gpd
+
+# define the points layers
+out_path = "/home/maita/Nextcloud/Documents/Work/Gap_Map/out/"
+pointfiles = {"fv":out_path + "fv.nstops.csv",
+          "all":out_path + "nstops.csv"}
+
+# define the area layers
+geo_path = "/home/maita/Nextcloud/Documents/Work/Gap_Map/raw/geo/"
+admin_area_path = geo_path + "vg250-ew_12-31.utm32s.shape.ebenen/vg250-ew_ebenen_1231/"
+shapefiles = {"gem":admin_area_path + "VG250_GEM.shp",
+              "kre":admin_area_path + "VG250_KRS.shp",
+              "lan":admin_area_path + "VG250_LAN.shp"}
+
+# define additional information tables
+sfl_tables= {"gem":geo_path + "Tabelle_Siedlungsflaeche_Gemeinde.csv",
+              "kre":geo_path + "Tabelle_Siedlungsflaeche_Kreis.csv",
+              "lan":geo_path + "Tabelle_Siedlungsflaeche_Land.csv"}
+
+for scope in pointfiles.keys():
+    for level in shapefiles.keys():
+        # what about the admin areas?
+        gem_path = shapefiles[level]
+        gem_gdf = gpd.read_file(gem_path)
+        
+        #gem_gdf.head()
+        #gem_gdf.crs
+        gem_gdf = gem_gdf.to_crs("epsg:3857")
+        #gem_gdf.columns
+        gem_gdf.reset_index(inplace=True) #index to identify individual shapes
+        
+        # reading in also the corresponding Siedlungsflaeche
+        gem_sfl_path = sfl_tables[level]
+        gem_sfl_df = pd.read_csv(gem_sfl_path, skiprows = [1], sep = ";", decimal = ",")
+        #gem_sfl_df.head()
+        
+        # try how projections of the points fit into this
+        ncounts_df = pd.read_csv(pointfiles[scope])
+        #ncounts_df.head()
+        ncounts_gdf = gpd.GeoDataFrame(ncounts_df,
+                                       geometry = gpd.points_from_xy(ncounts_df.stop_lon, ncounts_df.stop_lat),
+                                       crs="epsg:4326").to_crs("epsg:3857")
+        
+        # Merge SFL onto shape
+            ## checking what field makes most sense to use for merge
+        #[s for s in gem_gdf.AGS_0.astype('int64') if s in gem_sfl_df.Kennziffer]
+        
+        #sum(gem_sfl_df.Kennziffer.isin(gem_gdf.AGS_0.astype(int)))
+        #sum(gem_sfl_df.Kennziffer.isin(gem_gdf.AGS.astype(int)))
+        
+        #gem_sfl_df[~gem_sfl_df.Kennziffer.isin(gem_gdf.AGS_0.astype(int))][["Raumeinheit","Kennziffer"]]
+        #gem_gdf[~gem_gdf.AGS_0.astype(int).isin(gem_sfl_df.Kennziffer)][["GEN","AGS_0"]]
+            ## es scheint als ob einige Kennziffern sich nicht 100% decken
+        
+        gem_gdf.AGS = gem_gdf.AGS.astype(int)
+        gem_sfl_gdf = gem_gdf.merge(gem_sfl_df, how="left", left_on = "AGS", right_on = "Kennziffer")
+        # !!! actually do this at the very end
+        
+        
+        
+        # Merge shapes with pointcounts
+        sum_gdf = gpd.sjoin(gem_sfl_gdf, ncounts_gdf, how="left", op="contains" # spatial join
+                            )[["index", "n"]                                    # select only relevant cols
+                            ].groupby("index").sum()                            # groupby shapes and sum
+        agg_df = gem_sfl_gdf.merge(sum_gdf, how="left", on="index")            # join shapes & info back on
+        agg_df["SFL_frac"] = agg_df["Anteil Siedlungs- und Verkehrsfläche"]/100 # clean up SFL
+        
+        clean_df = agg_df[["AGS","GEN","EWZ", "KFL", "SFL_frac","n","geometry"]]
+            # select relevant cols
+        # clean_df.to_file(out_path + level +"."+scope+".stops.3857.geojson",driver="GeoJSON")
+        agg_df.to_file(out_path + level +"."+scope+".stops.3857.geojson",driver="GeoJSON")
+
+
 
 # load the grid layers
 gridfiles = {"50k":"/home/maita/Nextcloud/Documents/Work/Gap_Map/out/50k.3857.geojson",
         "5k":"/home/maita/Nextcloud/Documents/Work/Gap_Map/out/5k.3857.geojson",
         "0.5k":"/home/maita/Nextcloud/Documents/Work/Gap_Map/out/0.5k.3857.geojson"}
-
-# load the points layers
-pointfiles = {"fv":"/home/maita/Nextcloud/Documents/Work/Gap_Map/out/fv.nstops.csv",
-          "all":"/home/maita/Nextcloud/Documents/Work/Gap_Map/out/nstops.csv"}
-
-# what about the admin areas?
-geo_path = "/home/maita/Nextcloud/Documents/Work/Gap_Map/raw/geo/"
-admin_area_path = geo_path + "vg250-ew_12-31.utm32s.shape.ebenen/vg250-ew_ebenen_1231/"
-gem_path = admin_area_path + "VG250_GEM.shp"
-gem_gdf = gpd.read_file(gem_path)
-
-gem_gdf.head()
-gem_gdf.crs
-gem_gdf = gem_gdf.to_crs("epsg:3857")
-gem_gdf.columns
-
-# reading in also the corresponding Siedlungsflaeche
-gem_sfl_path = geo_path + "Tabelle_Siedlungsflaeche_Gemeinde.csv"
-gem_sfl_df = pd.read_csv(gem_sfl_path, skiprows = [1], sep = ";", decimal = ",")
-gem_sfl_df.head()
-
-# try how projections of the points fit into this
-out_path = "/home/maita/Nextcloud/Documents/Work/Gap_Map/out/"
-ncounts_df = pd.read_csv(out_path + "nstops.csv")
-ncounts_df.head()
-ncounts_gdf = gpd.GeoDataFrame(ncounts_df,
-                               geometry = gpd.points_from_xy(ncounts_df.stop_lon, ncounts_df.stop_lat),
-                               crs="epsg:4326").to_crs("epsg:3857")
-
-# Merge SFL onto shape
-[s for s in gem_gdf.AGS_0.astype('int64') if s in gem_sfl_df.Kennziffer]
-
-sum(gem_sfl_df.Kennziffer.isin(gem_gdf.AGS_0.astype(int)))
-sum(gem_sfl_df.Kennziffer.isin(gem_gdf.AGS.astype(int)))
-
-gem_sfl_df[~gem_sfl_df.Kennziffer.isin(gem_gdf.AGS_0.astype(int))][["Raumeinheit","Kennziffer"]]
-gem_gdf[~gem_gdf.AGS_0.astype(int).isin(gem_sfl_df.Kennziffer)][["GEN","AGS_0"]]
-    ## es scheint als ob einige Kennziffern sich nicht 100% decken
-
-gem_gdf.AGS = gem_gdf.AGS.astype(int)
-gem_sfl_gdf = gem_gdf.merge(gem_sfl_df, how="left", left_on = "AGS", right_on = "Kennziffer")
-
-# Merge shapes onto pointcounts
-joint_gdf = gpd.sjoin(gem_sfl_gdf, ncounts_gdf, how="left", op="contains")
-len(joint_gdf)
-len(gem_sfl_gdf)
-len(ncounts_gdf)
-joint_gdf.columns
-joint_gdf[["Raumeinheit","GEN"]]
-
-s = "ADE"
-[sum(gem_gdf[s].duplicated()) for s in gem_gdf.columns]
-
-joint_gdf[["geometry", "n"]].groupby("geometry").sum()
-joint_gdf[["AGS", "n"]].groupby("AGS").sum().merge(
-        joint_df.drop_, how=)
-
-agg_df = joint_gdf[["AGS","GEN","EWZ","geometry", "KFL", "Anteil Siedlungs- und Verkehrsfläche","n"]
-            ].groupby(["AGS","GEN","EWZ","geometry", "KFL", "Anteil Siedlungs- und Verkehrsfläche"]).sum()
 
 def read_process_points(fname):
     #    fname = pointfiles["fv"]
