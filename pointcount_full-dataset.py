0edff9274e890ef394d8b0e160501c2b791b9f5c
Maita Schade <maita.schade@agora-verkehrswende.de>
2021-04-22 02:14:16 +0200

    working version grid creation and counting in Germany bounding box

--- pointcount_full-dataset.py
+++ pointcount_full-dataset.py
@@ -17,7 +17,13 @@ import geopandas as gpd
 # define the points layers
 out_path = "/home/maita/Nextcloud/Documents/Work/Gap_Map/out/"
 pointfiles = {"all":out_path + "nstops.csv",
+
               "fv":out_path + "fv.nstops.csv"}
+
+###############
+    # shapes
+###############
+
 
 # define the area layers
 geo_path = "/home/maita/Nextcloud/Documents/Work/Gap_Map/raw/geo/"
@@ -91,52 +97,53 @@ for level in shapefiles.keys():
 ###############
     
 # make a grid -- one for each sidelength, in good projection
+# Here, all grids will be in EPSG3035, a Germany-centered EA-projection
 from shapely.geometry import Polygon
 import numpy as np
+
+# Germany bounding box
 world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres')).to_crs('epsg:3035')
-germany = world[world.name == "Germany"]
+germany = world[world.name == "Germany"] # we could do a grid around all points, 
+                                        # but that seems a bit excessive right now; it might be quicker this way
+                              
+                                        
 xmin,ymin,xmax,ymax =  germany.total_bounds
 
-sls = [50000, 100000] #m
+sls = [50, 10, 5, 1] # km
 grids = {}
-for sl in sls:
-    rows = int(np.ceil((ymax-ymin) /  sl))
-    cols = int(np.ceil((xmax-xmin) / sl))
+for scale in sls:
+    print("Making grid with sidelength "+ str(scale) + "km")
+    slm = scale*1000 # m
+    rows = int(np.ceil((ymax-ymin) /  slm)) 
+    cols = int(np.ceil((xmax-xmin) / slm))  
+    print(str(rows) + " x " + str(cols))
     XleftOrigin = xmin
-    XrightOrigin = xmin + sl
+    XrightOrigin = xmin + slm
     YtopOrigin = ymax
-    YbottomOrigin = ymax- sl
+    YbottomOrigin = ymax- slm
     polygons = []
     for i in range(cols):
         Ytop = YtopOrigin
         Ybottom =YbottomOrigin
         for j in range(rows):
             polygons.append(Polygon([(XleftOrigin, Ytop), (XrightOrigin, Ytop), (XrightOrigin, Ybottom), (XleftOrigin, Ybottom)])) 
-            Ytop = Ytop - sl
-            Ybottom = Ybottom - sl
-        XleftOrigin = XleftOrigin + sl
-        XrightOrigin = XrightOrigin + sl
+            Ytop = Ytop - slm
+            Ybottom = Ybottom - slm
+        XleftOrigin = XleftOrigin + slm
+        XrightOrigin = XrightOrigin + slm
     grid = gpd.GeoDataFrame({'geometry':polygons})
     grid.crs = 'epsg:3035'
-    grids[sl] = grid
+    grids[scale] = grid
     ax = grid.boundary.plot()
     germany.boundary.plot(ax=ax)
-
-# get points in good projection
-scope = "fv"
-points_df = pd.read_csv(pointfiles[scope])
-points_gdf = gpd.GeoDataFrame(points_df,
-                              geometry = gpd.points_from_xy(points_df.stop_lon, points_df.stop_lat),
-                              crs="epsg:4326").to_crs('epsg:3035') # apparently best projection for Germany
-ax = germany.boundary.plot()
-grids[50000].boundary.plot(ax=ax)
-points_gdf.plot(ax=ax)
-
+    
 # repeat the above exercise with the grid
 # get counts of stops in all shapes associated with each AGS
 for scale in sls:
+    print("Getting counts for grid with sidelength "+ str(scale) + "km")
     agg_counts_gdf = grids[scale]
     for scope in pointfiles.keys():
+        print("... counting "+scope"-stops ...")
         # read counts per station, with point locations
         ncounts_df = pd.read_csv(pointfiles[scope])
         ncounts_gdf = gpd.GeoDataFrame(ncounts_df,
@@ -147,8 +154,9 @@ for scale in sls:
                             ).drop("index_right",axis=1).reset_index().dissolve(by="index",aggfunc='sum').rename({"n":"n."+scope},axis=1)
     agg_counts_gdf = agg_counts_gdf[agg_counts_gdf['n.all']>0]
     agg_counts_gdf.to_crs('epsg:4326', inplace=True)
-    print("Writing "+ "{:.0f}".format(scale/1000,) +"k.stops.3857.geojson")
-    agg_counts_gdf.to_file(out_path + "{:.0f}".format(scale/1000,) +"k.stops.3857.geojson",driver="GeoJSON")
+    print("Writing "+ str(scale) +"k.stops.3857.geojson")
+    agg_counts_gdf.to_file(out_path + str(scale) +"k.stops.3857.geojson",driver="GeoJSON")
+
 
 # load the grid layers
 gridfiles = {"50k":"/home/maita/Nextcloud/Documents/Work/Gap_Map/out/50k.3857.geojson",
