7f390894f7bf85bf2240a4b9f59b16002e6ac592
Maita Schade <maita.schade@agora-verkehrswende.de>
2021-05-07 08:47:45 +0200

    changed to a) label rather than GEN as label field and b) put out just one file containing all levels

--- admin_areas_label.py
+++ admin_areas_label.py
@@ -5,7 +5,7 @@ Created on Fri May 7
 
 @author: maita
 
-Creates label geojson for the VG250 admin geometries (one output file per admin level)
+Creates label geojson for the VG250 admin geometries 
 """
 
 import geopandas as gpd
@@ -27,18 +27,22 @@ shapefiles = {"gem":admin_area_path + "VG250_GEM.shp",
               "kre":admin_area_path + "VG250_KRS.shp",
               "lan":admin_area_path + "VG250_LAN.shp"}
 
+centroids_gdf = gpd.GeoDataFrame(columns = ["label", "level", "centroid"], geometry = "centroid", crs = "epsg:3857")
 for level in shapefiles.keys():
+    print("labelling " + level + "...")
     # Read in admin areas
     area_path = shapefiles[level]
     area_gdf = gpd.read_file(area_path)
     # fix projection
     area_gdf = area_gdf.to_crs("epsg:3857")
     area_gdf.AGS = area_gdf.AGS.astype(int) # to compare to other sources, get reasonable type for AGS
-    ax = area_gdf.plot()
     # select correct shapes, and dissolve them by AGS/GEN
-    laender_gdf = area_gdf[(area_gdf.KFL>0)][["AGS","GEN","geometry"]].dissolve(by="AGS")
-    laender_gdf["centroid"] = laender_gdf.centroid
-    centroids_df = laender_gdf[["GEN", "centroid"]]
-    centroids_gdf = gpd.GeoDataFrame(centroids_df, geometry = 'centroid', crs="epsg:3857") 
-    centroids_gdf.to_file(out_path + level +".labels.3857.geojson",driver="GeoJSON")
-    print("Wrote " + level +".labels.3857.geojson")
+    agg_gdf = area_gdf[(area_gdf.KFL>0)][["AGS","GEN","geometry"]].dissolve(by="AGS")
+    agg_gdf["centroid"] = agg_gdf.centroid
+    agg_gdf["level"]  = level
+    agg_gdf.rename(columns = {'GEN':'label'}, inplace = True)
+    this_centroids_gdf = gpd.GeoDataFrame(agg_gdf[["label", "level", "centroid"]], geometry = 'centroid', crs="epsg:3857") 
+    centroids_gdf = centroids_gdf.append(this_centroids_gdf)
+
+centroids_gdf.to_file(out_path + "labels.3857.geojson",driver="GeoJSON")
+print("Wrote labels.3857.geojson")
